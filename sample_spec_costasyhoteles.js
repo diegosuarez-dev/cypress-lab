4ea63c3d2376a693702c5ee858a9282cbbe8755b
Diego Suárez <dsuarezleon@gmail.com>
2020-04-16 18:48:42 +0100

    modify 2 files

--- sample_spec_costasyhoteles.js
+++ sample_spec_costasyhoteles.js
@@ -8,6 +8,8 @@ describe('My First Test', function() {
 
         cy.get('div.calendar:nth-child(87) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > a:nth-child(27)').click({force : true})
 
-        //cy.get('button[id=search_button_hotelsearchwidget0]').click({force : true})
+        cy.get('button[id=search_button_hotelsearchwidget0]').click({force : true})
+
+        //Probando git
     })
 })
